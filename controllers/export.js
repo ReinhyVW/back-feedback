d5cea4964eeffbf70a765ac1632915f89ea4497c
John Doe <johndoe@example.com>
2023-08-14 19:28:42 -0500

    Fixing Export Columns

--- controllers/export.js
+++ controllers/export.js
@@ -7,13 +7,28 @@ const router = express.Router();
 router.get('/export', async (req, res) => {
     try {
         const { fromDate, toDate } = req.query;  // Use req.query for query parameters
-        
+
         const pool = await getConnection();
 
         const result = await pool.request()
             .input('fromDate', sql.Date, fromDate)
             .input('toDate', sql.Date, toDate)
-            .query('SELECT * FROM Feedback WHERE RecordDate BETWEEN @fromDate AND @toDate');
+            .query(`
+            SELECT
+                F.RecordId,
+                F.RecordDate,
+                F.RecordTime,
+                A.OptionName AS Answer,
+                R.ReasonName AS Reason,
+                U.MemberName AS UserName,
+                C.FacilityName AS CenterName
+            FROM Feedback F
+            INNER JOIN Answers A ON F.AnswerId = A.AnswerId
+            INNER JOIN Reasons R ON F.ReasonId = R.ReasonId
+            INNER JOIN Users U ON F.UserId = U.UserId
+            INNER JOIN Centers C ON U.CenterId = C.CenterId
+            WHERE F.RecordDate BETWEEN @fromDate AND @toDate
+        `);
 
         const download = result.recordset;
 
